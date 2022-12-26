cffe4673f04773b9911343b16739b8ace4cd7c66
Alberto Fabbri <3659963-Albi-F@users.noreply.gitlab.com>
2022-12-26 23:43:00 +0100

    Fix login screen menu

--- app/src/main/java/com/cohabit/inventory/LoginFragment.java
+++ app/src/main/java/com/cohabit/inventory/LoginFragment.java
@@ -3,7 +3,11 @@ package com.cohabit.inventory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -11,6 +15,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,7 +28,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements MenuProvider {
 
     private FragmentLoginBinding binding;
 
@@ -31,6 +39,12 @@ public class LoginFragment extends Fragment {
     private FirebaseAuth auth;
 
     private NavController navController;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ((MainActivity)getActivity()).removeMenu();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -58,6 +72,7 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
+
     }
 
     private void loginUser(String email, String password) {
@@ -65,7 +80,7 @@ public class LoginFragment extends Fragment {
         auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
-                Toast.makeText(getActivity(), "Login Successful", Toast.LENGTH_SHORT).show();
+                Log.d("Login", "Login Successful");
                 navController.navigate(R.id.action_Login_to_Home);
             }
         }).addOnFailureListener(e -> {
@@ -89,9 +104,29 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
+    public void onStop() {
+        super.onStop();
+        ((MainActivity)getActivity()).addMenu();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
 
+    @Override
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menu.clear();
+    }
+
+    @Override
+    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPrepareMenu(@NonNull Menu menu) {
+        menu.clear();
+    }
 }
--- app/src/main/java/com/cohabit/inventory/MainActivity.java
+++ app/src/main/java/com/cohabit/inventory/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
+import android.view.MenuInflater;
 import android.view.View;
 
+import androidx.core.view.MenuProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.NavInflater;
@@ -28,7 +31,7 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MenuProvider {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -44,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+        addMenuProvider(this);
 
         final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         final NavController navController = navHostFragment.getNavController();
@@ -68,31 +72,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_logout) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = findNavController(this, R.id.fragmentContainerView);
         return navController.navigateUp() || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            Navigation.findNavController(this, R.id.fragmentContainerView).navigate(R.id.action_Home_to_Login);
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    public void removeMenu() {
+        removeMenuProvider(this);
+    }
+
+    public void addMenu() {
+        addMenuProvider(this);
     }
 }
 
