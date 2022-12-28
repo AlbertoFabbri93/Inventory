8a0c5f79db7ee7a46c5a49272153926f73c58e11
Jens Vighagen <jvighagen@gmail.com>
2022-12-28 15:10:05 +0100

    added more to Return fragment.
    getting StringArray from String.xml
    binding spinners (dose not work for me)
    Adding array to spinners
    Setting adapter for spinners.
    
    Created onDestroyView() and onViewCreated()

--- app/src/main/java/com/cohabit/inventory/Return.java
+++ app/src/main/java/com/cohabit/inventory/Return.java
@@ -1,64 +1,66 @@
 package com.cohabit.inventory;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Return#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+
+import com.cohabit.inventory.databinding.FragmentNewItemBinding;
+
 public class Return extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentNewItemBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Return() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Return.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Return newInstance(String param1, String param2) {
-        Return fragment = new Return();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // Get string Array from strings.xml
+    String [] functionalities = getResources().getStringArray(R.array.functionalities_array);
+    String [] aesthetics = getResources().getStringArray(R.array.aesthetics_array);
+    String [] location = getResources().getStringArray(R.array.location_array);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_return, container, false);
+        binding = FragmentNewItemBinding.inflate(inflater, container, false);
+
+        // Binding spinner.
+        Spinner spinnerFunctionality = binding.spinnerFunctionality;
+        Spinner spinnerAesthetics = binding.spinnerAesthetics;
+        Spinner spinnerLocation = binding.spinnerLocation;
+
+        // Adding Array to spinner.
+        ArrayAdapter<String> adapterFunctionality = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item, functionalities);
+        ArrayAdapter<String> adapterAesthetics = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item, aesthetics);
+        ArrayAdapter<String> adapterLocation = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item, location);
+
+        // setting adapter for spinners.
+        adapterFunctionality.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFunctionality.setAdapter(adapterFunctionality);
+        spinnerAesthetics.setAdapter(adapterAesthetics);
+        spinnerLocation.setAdapter(adapterLocation);
+
+        return binding.getRoot();
+    }
+
+    //
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSubmit.setOnClickListener(view1 -> NavHostFragment.findNavController(Return.this)
+                .navigate(R.id.action_New_Item_to_Home));
+    }
+    //
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
