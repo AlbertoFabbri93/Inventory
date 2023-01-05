99afd9205103704200576975629c433e648aa3f0
Alberto Fabbri <3659963-Albi-F@users.noreply.gitlab.com>
2023-01-05 22:34:29 +0100

    Upload to Firebase

--- app/src/main/java/com/cohabit/inventory/Item.java
+++ app/src/main/java/com/cohabit/inventory/Item.java
@@ -1,26 +1,26 @@
 package com.cohabit.inventory;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class Item {
-    private String name;
-    private String product_category;
-    private String material_category;
-    private String functionality;
-    private String aesthetics;
-    private String color;
-    private String dimensions;
-    private String id;
+    public String product_category;
+    public String material_category;
+    public String functionality;
+    public String aesthetics;
+    public String color;
+    public String dimensions;
 
     public Item() {
+        // Default constructor required for calls to DataSnapshot.getValue(Item.class)
     }
 
-    public Item(String name, String product_category, String material_category, String functionality, String aesthetics, String color, String dimensions, String id) {
-        this.name = name;
+    public Item(String product_category, String material_category, String functionality, String aesthetics, String color, String dimensions) {
         this.product_category = product_category;
         this.material_category = material_category;
         this.functionality = functionality;
         this.aesthetics = aesthetics;
         this.color = color;
         this.dimensions = dimensions;
-        this.id = id;
     }
 }
--- app/src/main/java/com/cohabit/inventory/NewItemFragment.java
+++ app/src/main/java/com/cohabit/inventory/NewItemFragment.java
@@ -8,51 +8,49 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.cohabit.inventory.databinding.FragmentNewItemBinding;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewItemFragment extends Fragment {
 
     private FragmentNewItemBinding binding;
     private String[] options = {"Product Category", "Table", "Sofa", "Chair", "Bed"};
-    private String[] options1 = {"Material Category", "Wood", "Steel","Plastic"};
+    private String[] options1 = {"Material Category", "Wood", "Steel", "Plastic"};
     private String[] options2 = {"Functionality", "Needs repair", "Does not need repair"};
     private String[] options3 = {"Aesthetics", "Damaged", "Brand New"};
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentNewItemBinding.inflate(inflater, container, false);
 
-        EditText editText = binding.editText;
-        EditText editText1 =binding.editText1;
-        String userInputString = editText.getText().toString();
-        CharSequence userInput = editText.getText();
+        EditText editTextColor = binding.color;
+        EditText editTextDimension = binding.editText1;
+        String userInputString = editTextColor.getText().toString();
+        CharSequence userInput = editTextColor.getText();
         binding.imageView3.setImageResource(R.drawable.cohabit_logo);
-        Spinner spinner = binding.spinner;
-        Spinner spinner1 = binding.spinner1;
-        Spinner spinner2 = binding.spinner2;
-        Spinner spinner3 = binding.spinner3;
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_spinner_item, options);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_spinner_item, options1);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_spinner_item, options2);
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_spinner_item, options3);
+        Spinner spinnerProductCategory = binding.spinner;
+        Spinner spinnerMaterialCategory = binding.spinner1;
+        Spinner spinnerFunctionality = binding.spinner2;
+        Spinner spinnerAesthetics = binding.spinner3;
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, options);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, options1);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, options2);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, options3);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner1.setAdapter(adapter1);
-        spinner2.setAdapter(adapter2);
-        spinner3.setAdapter(adapter3);
-        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerProductCategory.setAdapter(adapter);
+        spinnerMaterialCategory.setAdapter(adapter1);
+        spinnerFunctionality.setAdapter(adapter2);
+        spinnerAesthetics.setAdapter(adapter3);
+
+        spinnerMaterialCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedOption = options1[position];
@@ -60,12 +58,13 @@ public class NewItemFragment extends Fragment {
                     //do something
                 }
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 // Do nothing
             }
         });
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerProductCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedOption = options[position];
@@ -79,19 +78,28 @@ public class NewItemFragment extends Fragment {
                 // Do nothing
             }
         });
+
+        binding.Save.setOnClickListener(view1 -> {
+
+            Toast.makeText(getActivity(), "Item saved", Toast.LENGTH_SHORT).show();
+            String productCategory = spinnerProductCategory.getSelectedItem().toString();
+            String materialCategory = spinnerMaterialCategory.getSelectedItem().toString();
+            String functionality = spinnerFunctionality.getSelectedItem().toString();
+            String aesthetics = spinnerAesthetics.getSelectedItem().toString();
+            String color = editTextColor.getText().toString();
+            String dimensions = editTextDimension.getText().toString();
+            Item item = new Item(productCategory, materialCategory, functionality, aesthetics, color, dimensions);
+            FirebaseDatabase.getInstance("https://cohabit-inventory-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("items").push().setValue(item);
+            NavHostFragment.findNavController(NewItemFragment.this).navigate(R.id.action_New_Item_to_Home);
+        });
+
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.Secondbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(NewItemFragment.this)
-                        .navigate(R.id.action_New_Item_to_Home);
-            }
-        });
+
     }
 
     @Override
